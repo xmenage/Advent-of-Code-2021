7334e040c26407f00411e97a3d8b5ad97745e82b
Xavier Ménage <xmenage@yahoo.com>
2021-12-10 22:43:37 +0100

    added day 6

--- Day 6 Lanternfish/src/part2.kt
+++ Day 6 Lanternfish/src/part2.kt
@@ -5,21 +5,20 @@ import kotlin.io.*
 fun main() {
     val numListString: String = File("data/day6-input.txt").readText()
     val numList = numListString.split(',').map { it.toInt() }
-    var fishes = IntArray(9) { 0 }
+    var fishes = LongArray(9) { 0 }
     val k = numList.groupingBy { it }.eachCount()
     k.forEach { numCount ->
-        fishes[numCount.key] = numCount.value
+        fishes[numCount.key] = numCount.value.toLong()
     }
     println(k)
     println(fishes.toList())
-    repeat(80) {
+    repeat(256) {
         val f0 = fishes[0]
         for (i in 0..7) {
             fishes[i] = fishes[i + 1]
         }
         fishes[6] += f0
         fishes[8] = f0
-        println(fishes.toList())
     }
     println(fishes.sum())
 }
